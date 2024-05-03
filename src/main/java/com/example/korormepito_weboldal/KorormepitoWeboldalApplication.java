363473ceafaa3ebf46135dfc6d366350b31a9cd5
BorbelyKazincziDora <kazinczidora25@gmail.com>
2024-05-03 20:35:16 +0200

    First commit -basic roots -navbar

---
+++ src/main/java/com/example/korormepito_weboldal/KorormepitoWeboldalApplication.java
@@ -0,0 +1,13 @@
+package com.example.korormepito_weboldal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorormepitoWeboldalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KorormepitoWeboldalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/korormepito_weboldal/KorormepitoWeboldalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.korormepito_weboldal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorormepitoWeboldalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
