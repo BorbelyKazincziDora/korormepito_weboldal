d5f0b128c1a2e3cbcdf7f26974979e60598aa04d
BorbelyKazincziDora <kazinczidora25@gmail.com>
2024-05-15 09:09:46 +0200

    footer

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomePageComponent} from "./components/home-page/home-page.component";
+import {AboutComponent} from "./components/about/about.component";
+import {ServicesComponent} from "./components/services/services.component";
+import {PortfolioComponent} from "./components/portfolio/portfolio.component";
+import {PricesComponent} from "./components/prices/prices.component";
+import {ReservationComponent} from "./components/reservation/reservation.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
-
+  {path: 'about', component: AboutComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'prices', component: PricesComponent},
+  {path: 'reservation', component: ReservationComponent},
+  {path: 'contact', component: ContactComponent},
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import '@radix-ui/themes/styles.css';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    PricesComponent,
+    ReservationComponent,
+    ContactComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
